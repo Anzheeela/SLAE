fab2cc8e55c588f9876d3f94ccfa31f383a4f2eb
Anzheeela <roza.harafytdinova555@gmail.com>
2024-02-04 04:10:08 +0300

    without tests

--- progonka/test/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anzheeela/Documents/Study/4sem/SLAU/SLAE/progonka/test
-# Build directory: /home/anzheeela/Documents/Study/4sem/SLAU/SLAE/progonka/test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("dim")
